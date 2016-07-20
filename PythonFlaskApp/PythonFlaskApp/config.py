6db37c4ccd6744a763af952fc87313af390cf04c
Raisa Pokrovskaya <MrAmbrish@gmail.com>
2016-07-20 17:00:39 +0530

    App

---
+++ PythonFlaskApp/PythonFlaskApp/config.py
@@ -0,0 +1,9 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+DOCUMENTDB_HOST = 'https://pythonflaskapp.documents.azure.com:443/'
+DOCUMENTDB_KEY = 'nZN9vHxGqcsbXDBav99Df11hzz90SU2zh3O3CLWAEdAJAHAIVWVM5F0n5IB2PZJJxj9lDrwqKdmGnojLwS4Few=='
+
+DOCUMENTDB_DATABASE = 'voting database'
+DOCUMENTDB_COLLECTION = 'voting collection'
+DOCUMENTDB_DOCUMENT = 'voting document'
